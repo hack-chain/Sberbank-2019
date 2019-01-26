09f6e7c741e1c7812e18f70b6f778e29bb4f3d9d
Dmitry <inyutin.da@phystech.edu>
2019-01-26 19:38:34 +0300

    add lamboc and rename main class

--- src/main/java/com/Sberbank/Sberbank/MainApplication.java
+++ src/main/java/com/Sberbank/Sberbank/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SberbankApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SberbankApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
--- src/test/java/com/Sberbank/Sberbank/MainApplicationTests.java
+++ src/test/java/com/Sberbank/Sberbank/MainApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SberbankApplicationTests {
+public class MainApplicationTests {
 
 	@Test
 	public void contextLoads() {
