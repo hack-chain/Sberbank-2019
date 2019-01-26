e17e9b035a34eff87b4f0623356d405ea32eb55f
Dmitry <inyutin.da@phystech.edu>
2019-01-26 17:51:29 +0300

    init

---
+++ src/main/java/com/Sberbank/Sberbank/SberbankApplication.java
@@ -0,0 +1,14 @@
+package com.Sberbank.Sberbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SberbankApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/Sberbank/Sberbank/SberbankApplicationTests.java
@@ -0,0 +1,17 @@
+package com.Sberbank.Sberbank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SberbankApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
